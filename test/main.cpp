dd119b214dcfd06430a1ead9df76825a6abb02e5
roy4801 <a82611141@gmail.com>
2020-01-18 21:22:53 +0800

    Add nicp

--- test/main.cpp
+++ test/main.cpp
@@ -3,43 +3,97 @@
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+
+#define WINDOW_W 1280
+#define WINDOW_H 720
+
+class Animation
+{
+public:
+    static const std::string path;
+
+    Animation() = delete;
+    Animation(std::string prefix, int count, std::string ext)
+    {
+        fps = 24;
+        nowDraw = 0;
+        totalCnt = count;
+        playState = false;
+        //
+        char thName[10];
+
+        for (int i = 1; i <= totalCnt; i++)
+        {
+            std::shared_ptr<sf::Texture> t = std::make_shared<sf::Texture>();
+            std::shared_ptr<sf::Sprite> s = std::make_shared<sf::Sprite>();
+            sprintf(thName, "%03d", i);
+            t->loadFromFile(Animation::path + prefix + thName + ext);
+            auto size = t->getSize();
+            s->setTexture(*t);
+            s->setScale(1280.f / size.x, 720.f / size.y);
+
+            tex_v.push_back(t);
+            sprite_v.push_back(s);
+        }
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        if (playState && clk.getElapsedTime().asSeconds() >= 1 / (float)fps)
+        {
+            // printf("%d/%d\n", nowDraw, totalCnt);
+            clk.restart();
+            if (++nowDraw >= totalCnt)
+                nowDraw = 0;
+        }
+        // printf("%d/%d\n", nowDraw, totalCnt);
+        window.draw(*sprite_v[nowDraw]);
+    }
+
+    void play()
+    {
+        playState = true;
+    }
+    void stop()
+    {
+        playState = false;
+    }
+
+    void reset()
+    {
+        nowDraw = 0;
+        playState = false;
+    }
+private:
+    std::vector<std::shared_ptr<sf::Texture>> tex_v;
+    std::vector<std::shared_ptr<sf::Sprite>> sprite_v;
+    sf::Clock clk;
+    int fps;
+    int nowDraw, totalCnt;
+    bool playState;
+};
+
+const std::string Animation::path = "frames/";
+
 int main()
 {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML window");
-
-    std::string path = "frames/";
-    const int num = 288; // 24 fps
-    std::vector<sf::Texture> tex_v;
-    std::vector<sf::Sprite> sprite_v;
-
+    sf::RenderWindow window(sf::VideoMode(WINDOW_W, WINDOW_H), "SFML window");
+    // window.setFramerateLimit(60);
     sf::Clock clock;
-
-    sf::Clock play_clk;
-    for(int i = 1; i <= num; i++)
-    {
-        sf::Texture t;
-        char thName[10];
-        sprintf(thName, "%03d", i);
-
-        t.loadFromFile(path + "nico-" + thName + ".jpg");
-
-        auto size = t.getSize();
-        sf::Sprite s(t);
-        s.setScale(1280.f/size.x, 720.f/size.y);
-        sprite_v.push_back(s);
-    }
 
     sf::Music nico;
     if(!nico.openFromFile("nico.ogg")) printf("Failed to open %s\n", "nico.mp3");
 
-    nico.play();
-    int nowCnt = 0;
+    Animation ani("nico-", 288, ".jpg");
+
+    // init
     clock.restart();
     // Start the game loop
     while (window.isOpen())
     {
-        while(clock.getElapsedTime().asSeconds() < 1/60.f);
+        while(clock.getElapsedTime().asSeconds() < 1/60.f)
+            sf::sleep(sf::milliseconds(5));
         clock.restart();
         // Process events
         sf::Event event;
@@ -49,17 +103,38 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        // Clear screen
-        window.clear();
 
-        // if(play_clk.getElapsedTime().asSeconds() >= 1/24.f)
-        // {
-        //     nowCnt++;
-        //     if(nowCnt >= sprite_v.size()) nowCnt %= sprite_v.size();
-        //     play_clk.restart();
-        // }
-        window.draw(sprite_v[nowCnt]);
-        // Update the window
+        static bool spacePrev = false;
+        static bool playing = false;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+        {
+            if(!spacePrev)
+            {
+                puts("Pressed");
+                playing = !playing;
+            }
+
+            if(playing)
+            {
+                nico.play();
+                ani.play();
+            }
+            else
+            {
+                nico.pause();
+                ani.stop();
+            }
+        }
+        spacePrev = sf::Keyboard::isKeyPressed(sf::Keyboard::Space);
+        //
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+        {
+            playing = false;
+            ani.reset();
+        }
+        /* Render */
+        window.clear();
+        ani.draw(window);
         window.display();
     }
 }
